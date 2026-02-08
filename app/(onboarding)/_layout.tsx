7a33b70870ea069a4ec8faac68f72148c06824d8
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-02-08 15:55:43 +0500

    Add login and signup screens with navigation and form handling

--- app/(onboarding)/_layout.tsx
+++ app/(onboarding)/_layout.tsx
@@ -7,8 +7,8 @@ export default function Layout() {
     return (
         <Stack>
             <Stack.Screen name="welcome" options={{ headerShown: false }} />
-            <Stack.Screen name="login" options={{ headerShown: false }} />
-            <Stack.Screen name="signup" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)/signup" options={{ headerShown: false }} />
         </Stack>
     );
 }
