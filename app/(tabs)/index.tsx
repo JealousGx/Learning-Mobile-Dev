e96430200fcc87bbe6219d0d574e2e6db32e12de
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-02-10 21:42:47 +0500

    chore: add react-native-svg and react-native-svg-transformer dependencies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,53 @@
 import { SimpleLineIcons } from "@expo/vector-icons";
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+
+import BedroomIcon from "@/assets/icons/categories/bedroom.svg";
+import DiningRoomIcon from "@/assets/icons/categories/dining-room.svg";
+import KitchenIcon from "@/assets/icons/categories/kitchen.svg";
+import LivingRoomIcon from "@/assets/icons/categories/living-room.svg";
+import OfficeIcon from "@/assets/icons/categories/office.svg";
 
 import { ImageCarousel } from "@/components/shared/image-carousel";
 import { BodyText, Heading } from "@/components/shared/text";
 import { CustomView } from "@/components/shared/view";
 import { Button } from "@/components/ui/button";
 
-import { FONT_SIZES, FONT_WEIGHTS, SPACING } from "@/styles/theme";
+import { COLORS, FONT_SIZES, FONT_WEIGHTS, SPACING } from "@/styles/theme";
+
+const CATEGORIES = [
+    {
+        id: 1,
+        name: "Living Room",
+        Icon: LivingRoomIcon,
+    },
+    {
+        id: 2,
+        name: "Office",
+        Icon: OfficeIcon,
+    },
+    {
+        id: 3,
+        name: "Bedroom",
+        Icon: BedroomIcon,
+    },
+    {
+        id: 4,
+        name: "Kitchen",
+        Icon: KitchenIcon,
+    },
+    {
+        id: 5,
+        name: "Dining Room",
+        Icon: DiningRoomIcon,
+    },
+];
+
+type Category = typeof CATEGORIES[number];
 
 export default function Home() {
+    const [activeCategoryIndex, setActiveCategoryIndex] = useState(1);
+
     return (
         <CustomView style={styles.container}>
             <Header />
@@ -18,11 +57,13 @@ export default function Home() {
                     require("@/assets/images/home/headline-1.webp"),
                     require("@/assets/images/home/headline-1.webp"),
                     require("@/assets/images/home/headline-1.webp"),
-                    require("@/assets/images/home/headline-1.webp")
+                    require("@/assets/images/home/headline-1.webp"),
                 ]}
                 height={132}
                 autoPlay
             />
+
+            <Categories categories={CATEGORIES} activeCategoryIndex={activeCategoryIndex} setActiveCategoryIndex={setActiveCategoryIndex} />
         </CustomView>
     );
 }
@@ -44,11 +85,39 @@ function Header() {
     );
 }
 
+function Categories({ categories, activeCategoryIndex, setActiveCategoryIndex }: { categories: Category[], activeCategoryIndex: number, setActiveCategoryIndex: React.Dispatch<React.SetStateAction<number>> }) {
+    return (
+        <View style={{ gap: SPACING.xs }}>
+            <Heading style={{ color: COLORS.dark.primary }}>Categories</Heading>
+
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                style={styles.categoriesContainer}
+            >
+                {categories.map((category) => (
+                    <TouchableOpacity
+                        key={category.id}
+                        onPress={() => setActiveCategoryIndex(category.id)}
+                        style={[styles.categoryIconContainer, { backgroundColor: activeCategoryIndex === category.id ? COLORS.dark.primary : COLORS.dark.secondary }]}>
+                        <category.Icon
+                            width={42}
+                            height={42}
+                            color={activeCategoryIndex === category.id ? COLORS.dark["primary-foreground"] : COLORS.dark.foreground}
+                            fill="none"
+                        />
+                    </TouchableOpacity>
+                ))}
+            </ScrollView>
+        </View>
+    );
+}
+
 const styles = StyleSheet.create({
     container: {
         paddingHorizontal: SPACING.lg,
         paddingTop: SPACING.sm,
-        gap: SPACING.lg,
+        gap: SPACING.xl,
     },
 
     header: {
@@ -64,5 +133,18 @@ const styles = StyleSheet.create({
     subheading: {
         fontSize: FONT_SIZES.sm,
         fontWeight: FONT_WEIGHTS.regular,
+    },
+
+    categoriesContainer: {
+        marginTop: SPACING.md,
+    },
+    categoryIconContainer: {
+        width: 72,
+        height: 72,
+        alignItems: "center",
+        padding: SPACING.md,
+        borderRadius: SPACING.md,
+        justifyContent: "center",
+        marginRight: SPACING.md,
     },
 });
