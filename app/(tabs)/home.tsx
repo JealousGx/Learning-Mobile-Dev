0eb38a445f89fe02623e5328476f5ba6e8522183
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-02-10 16:17:24 +0500

    Add IndexRedirect component to handle initial routing

--- app/(tabs)/home.tsx
+++
@@ -1,8 +0,0 @@
-import { BodyText } from "@/components/shared/text";
-import { CustomView } from "@/components/shared/view";
-
-export default function Home() {
-    return <CustomView style={{ flex: 1, height: "auto" }}>
-        <BodyText>This is home tab</BodyText>
-    </CustomView>
-}
