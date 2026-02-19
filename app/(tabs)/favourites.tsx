429a9b08dc5f25eb1737cad8b16db9683a3efc72
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-02-19 21:34:25 +0500

    feat: implement Favourites component with favorite items display and clear favorites functionality

--- app/(tabs)/favourites.tsx
+++ app/(tabs)/favourites.tsx
@@ -1,8 +1,113 @@
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { FlatList, Image, StyleSheet, View } from "react-native";
+
+import { Nav } from "@/components/shared/nav";
 import { BodyText } from "@/components/shared/text";
 import { CustomView } from "@/components/shared/view";
+import { Button } from "@/components/ui/button";
+
+import { useCartStore } from "@/store/cart-store";
+
+import { COLORS, SPACING } from "@/styles/theme";
 
 export default function Favourites() {
-    return <CustomView>
-        <BodyText>This is favourites tab</BodyText>
-    </CustomView>
+    const router = useRouter();
+
+    const favItems = useCartStore((s) => s.favoriteItems);
+    const addToCart = useCartStore((state) => state.addToCart);
+    const removeFav = useCartStore((state) => state.removeFromFavorites);
+
+    if (!favItems || favItems.length === 0) {
+        return (
+            <CustomView style={styles.container}>
+                <Nav onBack={() => router.back()} title="My Favourites" />
+
+                <View
+                    style={{
+                        flex: 1,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        gap: SPACING.sm,
+                    }}
+                >
+                    <MaterialCommunityIcons
+                        name="playlist-star"
+                        size={256}
+                        color={COLORS.dark.primary}
+                    />
+
+                    <BodyText>No favorite items yet</BodyText>
+                </View>
+            </CustomView>
+        );
+    }
+
+    return (
+        <CustomView style={styles.container}>
+            <Nav onBack={() => router.back()} title="My Favourites" rightComponent={<ClearFavListButton />} />
+
+            <FlatList
+                data={favItems}
+                horizontal={false}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={{ gap: SPACING.md }}
+                renderItem={({ item }) => (
+                    <View style={{ flexDirection: "row", paddingVertical: SPACING.md, gap: SPACING.md, alignItems: "center" }}>
+                        <Image
+                            source={item.image}
+                            style={{
+                                width: 108,
+                                height: 108,
+                                borderRadius: 8,
+                                backgroundColor: COLORS.dark.white,
+                            }}
+                        />
+
+                        <View style={{ flex: 1, gap: SPACING.sm }}>
+                            <BodyText numberOfLines={1} style={{ flex: 1, color: COLORS.dark.primary }}>{item.name}</BodyText>
+
+                            <BodyText numberOfLines={2}>{item.description}</BodyText>
+
+                            <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                                <BodyText style={{ color: COLORS.dark.primary }}>${item.price.toFixed(2)}</BodyText>
+
+                                <View style={{ flexDirection: "row", marginTop: SPACING.sm, alignItems: "center", justifyContent: "flex-end", gap: SPACING.sm }}>
+                                    <Button size="icon-sm" onPress={() => removeFav(item.id)}>
+                                        <MaterialCommunityIcons name="heart-outline" size={20} color={COLORS.dark.white} />
+                                    </Button>
+
+                                    <Button size="icon-sm" onPress={() => addToCart(item)}>
+                                        <MaterialCommunityIcons name="cart-outline" size={20} color={COLORS.dark.white} />
+                                    </Button>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                )}
+            />
+        </CustomView>
+    );
 }
+
+function ClearFavListButton() {
+    const clearFavList = useCartStore((state) => state.clearFavorites);
+
+    return (
+        <Button
+            size="icon-sm"
+            onPress={clearFavList}
+        >
+            <AntDesign name="delete" size={16} color={COLORS.dark.white} />
+        </Button>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: SPACING.lg,
+        paddingTop: SPACING.sm,
+        paddingBottom: SPACING.lg,
+        gap: SPACING.xl,
+    },
+});
