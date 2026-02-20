adbdcbaefe5a8cc14ddb194e8990f3330c7277e8
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-02-20 21:50:43 +0500

    feat: implement user redirection logic based on onboarding status and user authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,13 @@
 import { Redirect } from "expo-router";
 
+import { useUserStore } from "@/store/user-store";
+
 export default function IndexRedirect() {
+    const { user, hasOnboarded } = useUserStore();
+
+    if (!hasOnboarded) return <Redirect href="/(onboarding)" />;
+    if (!user) return <Redirect href="/(onboarding)/welcome" />;
+
     return <Redirect href="/(tabs)" />;
 
 }
